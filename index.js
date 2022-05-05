9b5bf9caa628d2191d416ba44b24649a41b1ed32
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-05 21:27:04 +0700

    Deploy to Heroku Server

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
     );
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   const accountSID = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSID, authToken);
