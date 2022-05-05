af9b6a945849d037c10a03fa1fa48da0fd03bc2a
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-05 19:51:07 +0700

    Added Basic Authentication

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ const port = 3000;
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
+app.use((req, res, next) => {
+  const auth = { login: "yourlogins", password: "yourpassword" };
+
+  const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+  const [login, password] = Buffer.from(b64auth, "base64")
+    .toString()
+    .split(":");
+
+  if (login && password && login === auth.login && password === auth.password)
+    return next();
+
+  res
+    .header("Content-Type", "application/json; charset=UTF-8")
+    .set("WWW-Authenticate", 'Basic realm="401"')
+    .status(401)
+    .send(
+      JSON.stringify(
+        {
+          errorCode: 401,
+          error: "Unauthorized",
+          message:
+            "The pages you are trying to access requires authentication. Please try again.",
+        },
+        null,
+        2
+      )
+    );
+});
+
 app.get("/", (req, res) => {
   res.send("hiii");
 });
