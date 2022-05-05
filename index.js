7f062e1cbc8c095566bb986a1c4c725e0c1d2a70
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-05 21:01:25 +0700

    Use Twilio API to Send Messages

--- index.js
+++ index.js
@@ -40,7 +40,45 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("hiii");
+  const accountSID = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require("twilio")(accountSID, authToken);
+
+  client.messages
+    .create({
+      to: `whatsapp:${process.env.TWILIO_RECIPIENT_NUMBER}`,
+      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+      body: process.env.TWILIO_MESSAGE,
+    })
+    .then(() =>
+      res.send(
+        JSON.stringify(
+          {
+            statusCode: 200,
+            code: "Ok",
+            message: "Message sent successfully.",
+          },
+          null,
+          2
+        )
+      )
+    )
+    .catch((err) => {
+      res.send(
+        JSON.stringify(
+          {
+            statusCode: 500,
+            code: "Internal Server Error",
+            message: err.message,
+          },
+          null,
+          2
+        )
+      );
+    })
+    .done();
+
+  res.send(process.env.TWILIO_MESSAGE.replace(/\\n/g, "\n"));
 });
 
 app.listen(port, () => {
