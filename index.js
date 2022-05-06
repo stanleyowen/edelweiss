d32a4e97de459df88dfecad4c2a4951c26bfffcc
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-06 10:17:35 +0700

    Added `helmet` and `express-rate-limit` Dependency

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+app.use(helmet());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use((req, res, next) => {
   res.header("Content-Type", "application/json; charset=UTF-8");
