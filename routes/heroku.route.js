a4d7395d1e8d68610ba10861f32abcd78f768561
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-08 22:51:09 +0700

    Support for `PATCH` Request to `env-vars` Route

--- routes/heroku.route.js
+++ routes/heroku.route.js
@@ -9,4 +9,16 @@ router.get("/env-vars", (req, res) => {
     .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
 });
 
+router.patch("/env-vars", (req, res) => {
+  const { body } = req;
+  heroku
+    .patch(`/apps/${process.env.HEROKU_APP_NAME}/config-vars`, {
+      body: {
+        ...body,
+      },
+    })
+    .then((app) => res.status(200).send(JSON.stringify(app, null, 2)))
+    .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
+});
+
 module.exports = router;
