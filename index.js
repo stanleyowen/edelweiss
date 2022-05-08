26a790f626a6b5ec7841b3b820368e9b1d8885a0
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-08 22:22:21 +0700

    Simplify Route Component

--- index.js
+++ index.js
@@ -69,45 +69,8 @@ app.use((req, res, next) => {
     );
 });
 
-app.get("/", (req, res) => {
-  const accountSID = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const client = require("twilio")(accountSID, authToken);
-
-  client.messages
-    .create({
-      to: `whatsapp:${process.env.TWILIO_RECIPIENT_NUMBER}`,
-      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
-      body: process.env.TWILIO_MESSAGE.replace(/\\n/g, "\n"),
-    })
-    .then(() => {
-      return res.status(200).send(
-        JSON.stringify(
-          {
-            statusCode: 200,
-            code: "Ok",
-            message: "Message sent successfully.",
-          },
-          null,
-          2
-        )
-      );
-    })
-    .catch((err) => {
-      return res.status(500).send(
-        JSON.stringify(
-          {
-            statusCode: 500,
-            code: "Internal Server Error",
-            message: err.message,
-          },
-          null,
-          2
-        )
-      );
-    })
-    .done();
-});
+const messagesRouter = require("./routes/messages.route");
+app.use("/", messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
---
+++ routes/messages.route.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  const accountSID = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const client = require("twilio")(accountSID, authToken);
+
+  client.messages
+    .create({
+      to: `whatsapp:${process.env.TWILIO_RECIPIENT_NUMBER}`,
+      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+      body: process.env.TWILIO_MESSAGE.replace(/\\n/g, "\n"),
+    })
+    .then(() => {
+      return res.status(200).send(
+        JSON.stringify(
+          {
+            statusCode: 200,
+            code: "Ok",
+            message: "Message sent successfully.",
+          },
+          null,
+          2
+        )
+      );
+    })
+    .catch((err) => {
+      return res.status(500).send(
+        JSON.stringify(
+          {
+            statusCode: 500,
+            code: "Internal Server Error",
+            message: err.message,
+          },
+          null,
+          2
+        )
+      );
+    })
+    .done();
+
+  //   res.send("Testing");
+});
+
+module.exports = router;
