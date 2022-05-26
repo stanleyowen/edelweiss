d146e842b5b40a51256b180ec918de97f48d6d72
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-05-26 17:12:41 +0700

    Deprecated WhatsApp Integration in Meantime

--- routes/messages.route.js
+++ routes/messages.route.js
@@ -5,41 +5,41 @@ router.get("/", (req, res) => {
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require("twilio")(accountSID, authToken);
 
-  client.messages
-    .create({
-      to: `whatsapp:${process.env.TWILIO_RECIPIENT_NUMBER}`,
-      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
-      body: process.env.TWILIO_MESSAGE.replace(/\\n/g, "\n"),
-    })
-    .then(() => {
-      return res.status(200).send(
-        JSON.stringify(
-          {
-            statusCode: 200,
-            code: "Ok",
-            message: "Message sent successfully.",
-          },
-          null,
-          2
-        )
-      );
-    })
-    .catch((err) => {
-      return res.status(500).send(
-        JSON.stringify(
-          {
-            statusCode: 500,
-            code: "Internal Server Error",
-            message: err.message,
-          },
-          null,
-          2
-        )
-      );
-    })
-    .done();
+  client.messages;
+  // .create({
+  //   to: `whatsapp:${process.env.TWILIO_RECIPIENT_NUMBER}`,
+  //   from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+  //   body: process.env.TWILIO_MESSAGE.replace(/\\n/g, "\n"),
+  // })
+  // .then(() => {
+  //   return res.status(200).send(
+  //     JSON.stringify(
+  //       {
+  //         statusCode: 200,
+  //         code: "Ok",
+  //         message: "Message sent successfully.",
+  //       },
+  //       null,
+  //       2
+  //     )
+  //   );
+  // })
+  // .catch((err) => {
+  //   return res.status(500).send(
+  //     JSON.stringify(
+  //       {
+  //         statusCode: 500,
+  //         code: "Internal Server Error",
+  //         message: err.message,
+  //       },
+  //       null,
+  //       2
+  //     )
+  //   );
+  // })
+  // .done();
 
-  //   res.send("Testing");
+  res.send("Whatsapp Route is Deprecated for this Moment.");
 });
 
 module.exports = router;
