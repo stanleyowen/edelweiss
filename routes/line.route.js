aa6429c6ffb0ff532395f88317ac8d208ceb16cb
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-11 12:46:26 +0700

    Migrate line webhook from `Pipedream` server to `Edelweiss` Server

--- routes/line.route.js
+++ routes/line.route.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const axios = require("axios");
 const line = require("@line/bot-sdk");
 
 const client = new line.Client({
@@ -7,8 +8,18 @@ const client = new line.Client({
 const clientDestination = process.env.LINE_DESTINATION_ID.split(",");
 
 router.post("/webhook", (req, res) => {
-  console.log(req.body?.events[0]);
-  console.log("hi");
+  axios.post("https://eo22dsoei0ug3vt.m.pipedream.net", req.body).then(() => {
+    return res.status(200).send(
+      JSON.stringify(
+        {
+          statusCode: 200,
+          code: "Ok",
+        },
+        null,
+        2
+      )
+    );
+  });
 });
 
 router.get("/:id", (req, res) => {
