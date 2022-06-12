8e658c8c07028f9a6634eb2e8062955a18b28979
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-12 14:36:44 +0700

    Parse data from `pipedream` server

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const axios = require("axios");
 
 router.get("/", (_, res) => {
   return res.status(200).send(
@@ -12,6 +13,22 @@ router.get("/", (_, res) => {
       2
     )
   );
+});
+
+router.get("/pipedream", (req, res) => {
+  axios
+    .get(
+      "https://api.pipedream.com/v1/sources/dc_zqu8VnW/event_summaries?expand=event",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PIPEDREAM_API_KEY}`,
+        },
+      }
+    )
+    .then((logs) =>
+      res.status(200).send(JSON.stringify(logs.data?.data, null, 2))
+    )
+    .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
 });
 
 module.exports = router;
