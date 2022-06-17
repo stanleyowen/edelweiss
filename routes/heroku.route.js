2eb0d911279d503be3ec6235e9ba2a259e4cb389
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-17 13:04:44 +0700

    Support for dynamics path for env router

--- routes/heroku.route.js
+++ routes/heroku.route.js
@@ -2,17 +2,17 @@ const router = require("express").Router();
 const Heroku = require("heroku-client");
 const heroku = new Heroku({ token: process.env.HEROKU_API_KEY });
 
-router.get("/env-vars", (_, res) => {
+router.get("/env-vars/:app_name", (req, res) => {
   heroku
-    .get(`/apps/${process.env.HEROKU_APP_NAME}/config-vars`)
+    .get(`/apps/${req.params.app_name}/config-vars`)
     .then((env) => res.status(200).send(JSON.stringify(env, null, 2)))
     .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
 });
 
-router.patch("/env-vars", (req, res) => {
+router.patch("/env-vars/:app_name", (req, res) => {
   const { body } = req;
   heroku
-    .patch(`/apps/${process.env.HEROKU_APP_NAME}/config-vars`, {
+    .patch(`/apps/${req.params.app_name}/config-vars`, {
       body,
     })
     .then((env) => res.status(200).send(JSON.stringify(env, null, 2)))
