c4f2fbf84a97f9608be19ae36bfabccddd7e639e
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-17 11:24:37 +0700

    Added ability for deleting `pipedream` responses

--- routes/main.route.js
+++ routes/main.route.js
@@ -37,4 +37,26 @@ router.get("/pipedream", (req, res) => {
     .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
 });
 
+router.delete("/pipedream", (req, res) => {
+  axios
+    .delete("https://api.pipedream.com/v1/sources/dc_zqu8VnW/events", {
+      headers: {
+        Authorization: `Bearer ${process.env.PIPEDREAM_API_KEY}`,
+      },
+    })
+    .then(() =>
+      res.status(200).send(
+        JSON.stringify(
+          {
+            statusCode: 200,
+            code: "Ok",
+          },
+          null,
+          2
+        )
+      )
+    )
+    .catch((err) => res.status(400).send(JSON.stringify(err, null, 2)));
+});
+
 module.exports = router;
