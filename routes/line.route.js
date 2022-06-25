d43240218114b2a4cf612fbfdaddb1bdcb23f459
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-25 22:40:10 +0700

    Split words into word and validate keywords category

--- routes/line.route.js
+++ routes/line.route.js
@@ -15,13 +15,14 @@ const clientDestination = process.env.LINE_DESTINATION_ID.split(",");
 
 function removeDuplicates(str) {
   // Split words into an array
-  const sentences = [];
-  const words = str.split(" ");
-
   // Remove duplicates from each word and push to sentences
-  words.forEach((word) => {
-    sentences.push([...new Set(word.split(""))].join(""));
-  });
+  // Remove punctuation from each word and push to sentences except for the '/'
+  const sentences = [];
+  const words = str.replace(/[^\w\s\/]/gi, "").split(" ");
+
+  words.forEach((word) =>
+    sentences.push([...new Set(word.split(""))].join(""))
+  );
 
   return sentences.join(" ");
 }
@@ -30,60 +31,28 @@ router.post("/webhooks", (req, res) => {
   // Check if request body object is not null
   if (Object.keys(req.body).length > 0) {
     // Removes duplicate characters from the string
-    const text = removeDuplicates(req.body.events[0].message.text);
+    // Split words into an array including punctuation
+    const text = removeDuplicates(req.body.events[0].message.text).split(" ");
+    let idx = 0;
 
     if (process.env.NODE_ENV !== "development")
       axios.post(`${process.env.WEBHOOK_URL}/line`, req.body);
 
-    if (validateKeywords("okay", text))
-      replayMessageReaction("okay", req.body, (cb) => {
-        res.status(cb.statusCode).send(JSON.stringify(cb, null, 2));
-      });
-    else if (validateKeywords("laugh", text))
-      replayMessageReaction("laugh", req.body, (cb) => {
-        res.status(cb.statusCode).send(JSON.stringify(cb, null, 2));
-      });
-    else
-      return res.status(200).send(
-        JSON.stringify(
-          {
-            statusCode: 200,
-            statusMessage: "Ok",
-          },
-          null,
-          2
-        )
-      );
-  } else if (
-    text.toLowerCase().includes("thanks") ||
-    (text.toLowerCase().split("t").length - 1 > 1 &&
-      text.toLowerCase().split("k").length - 1 > 1)
-  ) {
-    const stickerIndex = Math.floor(Math.random() * stickers.laugh.length);
-    client
-      .replyMessage(req.body.events[0].replyToken, {
-        type: "sticker",
-        packageId: stickers.laugh[stickerIndex].packageId,
-        stickerId: stickers.laugh[stickerIndex].stickerId,
-      })
-      .then(() => {
-        return res.status(200).send(
-          JSON.stringify(
-            {
-              statusCode: 200,
-              statusMessage: "Ok",
-              message: "Reply Message sent successfully.",
-            },
-            null,
-            2
-          )
+    while (
+      (validateKeywords("okay", text[idx]) ||
+        validateKeywords("laugh", text[idx])) &&
+      idx < text.length
+    ) {
+      if (validateKeywords("okay", text[idx]))
+        replayMessageReaction("okay", req.body, (cb) =>
+          res.status(cb.statusCode).send(JSON.stringify(cb, null, 2)).end()
         );
-      })
-      .catch((err) => {
-        errorReporter(err);
-        res.status(err.statusCode ?? 400).send(JSON.stringify(err, null, 2));
-      });
-  } else
+      else if (validateKeywords("laugh", text[idx]))
+        replayMessageReaction("laugh", req.body, (cb) =>
+          res.status(cb.statusCode).send(JSON.stringify(cb, null, 2)).end()
+        );
+      idx++;
+    }
     return res.status(200).send(
       JSON.stringify(
         {
@@ -111,18 +80,23 @@ router.post("/webhookss", (req, res) => {
   //       res.status(err.statusCode ?? 400).send(JSON.stringify(err, null, 2));
   //     });
   // });
-  return res.status(200).send(
-    JSON.stringify(
-      {
-        statusCode: 200,
-        statusMessage: "Ok",
-        isOkay: validateKeywords("okay", text),
-        isLaught: validateKeywords("laugh", text),
-      },
-      null,
-      2
-    )
-  );
+  // replayMessageReaction(validateKeywords("okay", text), req.body, (cb) => {
+  //   res.status(cb.statusCode).send(JSON.stringify(cb, null, 2));
+  console.log(validateKeywords("laugh", text));
+  // });
+  // } else
+  //   return res.status(200).send(
+  //     JSON.stringify(
+  //       {
+  //         statusCode: 200,
+  //         statusMessage: "Ok",
+  //         isOkay: validateKeywords("okay", text),
+  //         isLaught: validateKeywords("laugh", text),
+  //       },
+  //       null,
+  //       2
+  //     )
+  //   );
 });
 
 router.get("/:id", (req, res) => {
