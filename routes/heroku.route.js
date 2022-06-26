497a62bc6d86c91e0250a633e653f5d481bace91
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-26 13:15:00 +0700

    Refactor `heroku` operations to `lib`

--- routes/heroku.route.js
+++ routes/heroku.route.js
@@ -1,6 +1,8 @@
-const { getHerokuEnvVars } = require("../lib/herokuOperation");
-
 const router = require("express").Router();
+const {
+  getHerokuEnvVars,
+  updateHerokuEnvVars,
+} = require("../lib/herokuOperation");
 
 router.get("/env-vars/:app_name", (req, res) =>
   getHerokuEnvVars(req.params.app_name, (cb) =>
@@ -8,17 +10,10 @@ router.get("/env-vars/:app_name", (req, res) =>
   )
 );
 
-router.patch("/env-vars/:app_name", (req, res) => {
-  const { body } = req;
-  heroku
-    .patch(`/apps/${req.params.app_name}/config-vars`, {
-      body,
-    })
-    .then((env) => res.status(200).send(JSON.stringify(env, null, 2)))
-    .catch((err) => {
-      errorReporter(err);
-      res.status(err.statusCode ?? 400).send(JSON.stringify(err, null, 2));
-    });
-});
+router.patch("/env-vars/:app_name", (req, res) =>
+  updateHerokuEnvVars(req.params.app_name, req.body, (cb) =>
+    res.status(cb.statusCode).send(JSON.stringify(cb.data, null, 2))
+  )
+);
 
 module.exports = router;
