3f33a231a6c3c7bfc44bff43abccf2c547b6b2f5
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2022-06-26 13:08:20 +0700

    Added `getHerokuEnvVars()` function

--- routes/heroku.route.js
+++ routes/heroku.route.js
@@ -1,17 +1,12 @@
-const router = require("express").Router();
-const Heroku = require("heroku-client");
-const errorReporter = require("../lib/errorReporter");
-const heroku = new Heroku({ token: process.env.HEROKU_API_KEY });
+const { getHerokuEnvVars } = require("../lib/herokuOperation");
 
-router.get("/env-vars/:app_name", (req, res) => {
-  heroku
-    .get(`/apps/${req.params.app_name}/config-vars`)
-    .then((env) => res.status(200).send(JSON.stringify(env, null, 2)))
-    .catch((err) => {
-      errorReporter(err);
-      res.status(err.statusCode ?? 400).send(JSON.stringify(err, null, 2));
-    });
-});
+const router = require("express").Router();
+
+router.get("/env-vars/:app_name", (req, res) =>
+  getHerokuEnvVars(req.params.app_name, (cb) =>
+    res.status(cb.statusCode).send(JSON.stringify(cb.data, null, 2))
+  )
+);
 
 router.patch("/env-vars/:app_name", (req, res) => {
   const { body } = req;
