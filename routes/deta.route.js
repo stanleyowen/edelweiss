0d9e3d399df1f06faa26f43996ba29362223e6a6
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-04-21 16:58:09 +0700

    deprecate Heroku usage

--- routes/deta.route.js
+++ routes/deta.route.js
@@ -9,14 +9,14 @@ router.get("/", (req, res) =>
 
 router.put("/", (req, res) =>
   // Get the key from the first property of the body object
-  putData(req.body, Object.keys(req.body)[0], req.query, (cb) =>
+  putData(req.body, req.query, (cb) =>
     res.status(cb.statusCode).send(JSON.stringify(cb.data, null, 2))
   )
 );
 
 router.delete("/:key", (req, res) =>
   deleteData(req.params.key, (cb) =>
-    res.status(cb.statusCode).send(JSON.stringify(cb.data, null, 2))
+    res.status(cb.statusCode).send(JSON.stringify(cb, null, 2))
   )
 );
 
